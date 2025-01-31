b7f8c6bdaed2d6346ee31152152ffde3596b5872
jsonwamuyu <jysonmuchiri@gmail.com>
2025-01-31 14:43:03 +0300

    Added percel

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,13 +1,11 @@
 /// <reference types="Cypress"/>
 
-describe('Test login page', () => {
-
+describe("Test login page", () => {
   // beforeEach(()=>{
   //   cy.visit('login.html')
   // })
 
-  
-  it("Return header with content Login", ()=>{
-cy.visit('login.html')
-  })
-})
+  it("Return header with content Login", () => {
+    cy.visit("login.html");
+  });
+});
